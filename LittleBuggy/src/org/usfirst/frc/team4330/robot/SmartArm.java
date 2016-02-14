a809207a26faea92edee82c4fea88bfcdd1ea060
Randy Tielking <randy.lynn.tielking@monsanto.com>
2016-02-13 20:12:47 -0600

    configured smartarm encoder to report angular position in degrees

--- LittleBuggy/src/org/usfirst/frc/team4330/robot/SmartArm.java
+++ LittleBuggy/src/org/usfirst/frc/team4330/robot/SmartArm.java
@@ -5,6 +5,7 @@ import java.util.TimerTask;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
@@ -15,6 +16,7 @@ public class SmartArm {
 	private static final double D = 0;
 	private boolean isInitialzied;
 
+	// degrees with 0 at the raised hard stop and positive direction towards lowering
 	private static final double RAISE_POSITION = 20;
 	private static final double LOWER_POSITION = 3;
 	
@@ -28,11 +30,19 @@ public class SmartArm {
 
 	public SmartArm() {
 		speedController = new Talon(RobotMap.TREXARM_PORT);
+		
 		encoder = new Encoder(RobotMap.ARM_ENCODER_PORT_ONE,
 				RobotMap.ARM_ENCODER_PORT_TWO);
+		// want the encoder to report in angular position in degrees ( 360 degrees per revolution )
+		// and the encoder being used has 250 cycles (1000 ticks) per revolution
+		encoder.setDistancePerPulse(360.0 / 250);
+		encoder.setPIDSourceType(PIDSourceType.kDisplacement);
+		
+		// don't enable the PID controller until the encoder has been calibrated
 		positionController = new PIDController(P, I, D, 0, encoder,
 				speedController);
 		positionController.disable();
+		
 		isInitialzied = false;
 	}
 
@@ -65,15 +75,16 @@ public class SmartArm {
 
 	private class DetermineIfCalibrationComplete extends TimerTask {
 
+		// initialize last position read to a random big number
 		private double lastPositionRead = 1000;
 
 		@Override
 		public void run() {
-			double positionRead = encoder.pidGet();
+			double currentPositionRead = encoder.pidGet();
 			System.out.println("Arm position last=" + lastPositionRead
-					+ " current=" + positionRead);
+					+ " current=" + currentPositionRead);
 			final double angleTolerance = 1;
-			if (Math.abs(lastPositionRead - positionRead) < angleTolerance) {
+			if (Math.abs(lastPositionRead - currentPositionRead) < angleTolerance) {
 				// we have reached the up position, so set the encoder position
 				// to 0 degrees, deenergize the motor,
 				// change to pid control of the motor
@@ -82,11 +93,12 @@ public class SmartArm {
 				isInitialzied = true;
 				positionController.enable();
 				raise();
-				this.cancel();
+				// cancel this timer task since calibration has been completed
+				cancel();
 				System.out.println("Arm calibration complete");
 			} else {
 				// keep going
-				lastPositionRead = positionRead;
+				lastPositionRead = currentPositionRead;
 			}
 		}
 
