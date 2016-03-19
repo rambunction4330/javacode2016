942af9d4f92623c7a3d6869b0dcfd5e8f4be97ec
sparks58 <sparks10.sg@gmail.com>
2016-03-18 23:00:28 -0500

    added pneumatics

---
+++ Lil' Buggy Protobot/src/org/usfirst/frc/team4330/robot/Pneumatics.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team4330.robot;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+
+public class Pneumatics extends RobotMap {
+
+	private static Pneumatics instance;
+
+	private Timer time;
+	protected final Compressor comp;
+	protected final Solenoid kicker;
+
+	public static Pneumatics getInstance() {
+		if (instance == null) {
+			instance = new Pneumatics();
+		}
+		return instance;
+	}
+
+	private Pneumatics() {
+		comp = new Compressor(COMPRESSOR);
+		
+		comp.start();
+
+		kicker = new Solenoid(KICKER_SOL);
+		time = new Timer();
+	}
+
+	public void KickerOut() {
+		kicker.set(true);
+	}
+
+	public void KickerIn() {
+		kicker.set(true);
+	}
+
+	public void start() {
+		time.reset();
+		time.start();
+	}
+
+	public double getTime() {
+		return time.get();
+	}
+	
+}
